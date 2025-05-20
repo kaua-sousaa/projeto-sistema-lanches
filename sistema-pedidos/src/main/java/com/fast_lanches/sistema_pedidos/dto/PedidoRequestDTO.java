8ea5e84a6eef4d67b77613473f96e42e75e6bdad
kaua-sousaa <kauasousa15_rj@hotmail.com>
2025-05-20 11:33:24 -0300

    adicao de id e status em dto pedido

--- sistema-pedidos/src/main/java/com/fast_lanches/sistema_pedidos/dto/PedidoRequestDTO.java
+++ sistema-pedidos/src/main/java/com/fast_lanches/sistema_pedidos/dto/PedidoRequestDTO.java
@@ -2,6 +2,7 @@ package com.fast_lanches.sistema_pedidos.dto;
 
 import java.util.List;
 import com.fast_lanches.sistema_pedidos.enums.MetodoPagamento;
+import com.fast_lanches.sistema_pedidos.enums.Status;
 import com.fast_lanches.sistema_pedidos.model.Cliente;
 import com.fast_lanches.sistema_pedidos.model.Endereco;
 
@@ -11,10 +12,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PedidoRequestDTO {
-    
+    private long id;
     private Cliente cliente;
     private Endereco endereco;
     private MetodoPagamento metodoPagamento;
+    private Status status;
     private long lanchoneteId;
     private List<ItemPedidoRequestDTO> itensPedido;
 }
